8c4eae4acb655c99a3db6c268b6021a0f1f4ecda
unknown <shayan.sepasdarshoshtari@ontariotechu.net>
2022-11-10 16:28:40 -0500

    add all my files

--- Day1/1/Measurment.py
+++ Day1/1/Measurment.py
@@ -1,7 +1,7 @@
 with open('Day1/1/input.txt') as f:
     for lines in f:
         lines = f.readlines() # open and read the file per line
-        numbers = [int(e.strip()) for e in lines] # store each line in a list
+        numbers = [int(e.strip()) for e in lines] # store each line as int in a list
         count = 1
         for i,j in zip(numbers, numbers[1:]): # two poiner if statement to compare two indexes
                 if j > i:
